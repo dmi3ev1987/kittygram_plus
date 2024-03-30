fe82731359fe60b6aefdab3bb0c84f7a65e2b2a2
Андрей Дмитриев <andrey.dmi3ev@gmail.com>
2024-03-30 14:46:08 +0300

    JSON Web Token.

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+# from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
@@ -10,4 +11,11 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # path('api-token-auth/', views.obtain_auth_token),
+
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
